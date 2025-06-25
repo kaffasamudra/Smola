545b451e886099a676a555cfade5e900d777deab
kaffasamudra <kaffas87@gmail.com>
2025-06-25 22:48:43 +0700

    foto paud

--- application/views/ortu/dashboard.php
+++ application/views/ortu/dashboard.php
@@ -69,7 +69,7 @@
     <div class="container-fluid p-0 mb-5">
         <div class="owl-carousel header-carousel position-relative">
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="<?= base_url("assets/users/") ?>img/foto paud.jpg" alt="">
+                <img class="img-fluid" src="<?= base_url("assets/users/") ?>img/foto paud 2.png" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
                     <div class="container">
                         <div class="row justify-content-start">
@@ -85,7 +85,7 @@
                 </div>
             </div>
             <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="<?= base_url("assets/users/") ?>img/foto paud 1.jpg" alt="">
+                <img class="img-fluid" src="<?= base_url("assets/users/") ?>img/foto paud 1.png" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(24, 29, 56, .7);">
                     <div class="container">
                         <div class="row justify-content-start">
